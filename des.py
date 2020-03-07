881a55dd7d58f8dcb0332c17486bdf393225b9dd
Eduard Budniak <comp.disk2013@yandex.by>
2020-03-07 21:13:28 +0300

    des

---
+++ des.py
@@ -0,0 +1,257 @@
+import struct
+from typing import List, Any
+
+from bitarray import bitarray
+
+# Permutation and translation tables for DES
+PC1 = [56, 48, 40, 32, 24, 16, 8,
+       0, 57, 49, 41, 33, 25, 17,
+       9, 1, 58, 50, 42, 34, 26,
+       18, 10, 2, 59, 51, 43, 35,
+       62, 54, 46, 38, 30, 22, 14,
+       6, 61, 53, 45, 37, 29, 21,
+       13, 5, 60, 52, 44, 36, 28,
+       20, 12, 4, 27, 19, 11, 3
+       ]
+
+# number left rotations of pc1
+LEFT_SHIFT = [
+    1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1
+]
+
+# permuted choice key (table 2)
+PC2 = [
+    13, 16, 10, 23, 0, 4,
+    2, 27, 14, 5, 20, 9,
+    22, 18, 11, 3, 25, 7,
+    15, 6, 26, 19, 12, 1,
+    40, 51, 30, 36, 46, 54,
+    29, 39, 50, 44, 32, 47,
+    43, 48, 38, 55, 33, 52,
+    45, 41, 49, 35, 28, 31
+]
+
+# initial permutation IP
+IP = [57, 49, 41, 33, 25, 17, 9, 1,
+      59, 51, 43, 35, 27, 19, 11, 3,
+      61, 53, 45, 37, 29, 21, 13, 5,
+      63, 55, 47, 39, 31, 23, 15, 7,
+      56, 48, 40, 32, 24, 16, 8, 0,
+      58, 50, 42, 34, 26, 18, 10, 2,
+      60, 52, 44, 36, 28, 20, 12, 4,
+      62, 54, 46, 38, 30, 22, 14, 6
+      ]
+
+# Expansion table for turning 32 bit blocks into 48 bits
+E = [
+    31, 0, 1, 2, 3, 4,
+    3, 4, 5, 6, 7, 8,
+    7, 8, 9, 10, 11, 12,
+    11, 12, 13, 14, 15, 16,
+    15, 16, 17, 18, 19, 20,
+    19, 20, 21, 22, 23, 24,
+    23, 24, 25, 26, 27, 28,
+    27, 28, 29, 30, 31, 0
+]
+
+# The (in)famous S-boxes
+S = [
+    # S1
+    [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+     0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+     4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+     15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],
+
+    # S2
+    [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+     3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+     0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+     13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],
+
+    # S3
+    [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+     13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+     13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+     1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],
+
+    # S4
+    [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+     13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+     10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+     3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],
+
+    # S5
+    [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+     14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+     4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+     11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],
+
+    # S6
+    [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+     10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+     9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+     4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],
+
+    # S7
+    [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+     13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+     1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+     6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],
+
+    # S8
+    [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+     1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+     7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+     2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11],
+]
+
+P = [
+    15, 6, 19, 20, 28, 11,
+    27, 16, 0, 14, 22, 25,
+    4, 17, 30, 9, 1, 7,
+    23, 13, 31, 26, 2, 8,
+    18, 12, 29, 5, 21, 10,
+    3, 24
+]
+
+FP = [
+    39, 7, 47, 15, 55, 23, 63, 31,
+    38, 6, 46, 14, 54, 22, 62, 30,
+    37, 5, 45, 13, 53, 21, 61, 29,
+    36, 4, 44, 12, 52, 20, 60, 28,
+    35, 3, 43, 11, 51, 19, 59, 27,
+    34, 2, 42, 10, 50, 18, 58, 26,
+    33, 1, 41, 9, 49, 17, 57, 25,
+    32, 0, 40, 8, 48, 16, 56, 24
+]
+
+
+def create_bitarray():
+    return bitarray(endian='little')
+
+
+def permutate_arr(arr: bitarray, permutation: List[Any]):
+    new_arr = create_bitarray()
+    for i in permutation:
+        new_arr.append(arr[i])
+    return new_arr
+
+
+def cycle_shift_left(arr: bitarray, count: int):
+    new_arr = create_bitarray()
+    n = len(arr)
+    for i in range(n):
+        new_arr.append(arr[(i - count + n) % n])
+    return new_arr
+
+
+def f(r: bitarray, k: bitarray):
+    extended = permutate_arr(r, E)
+    extended ^= k
+
+    final = create_bitarray()
+
+    for i in range(8):
+        b = extended[6 * i:6 * (i + 1)]
+        value = S[(b[:1] + b[-1:]).tobytes()[0]][b[1:-1].tobytes()[0]]
+        b = create_bitarray()
+        b.frombytes(bytes([value]))
+        final.extend(b)
+
+    return permutate_arr(final, P)
+
+
+def generate_keys(original: bitarray):
+    r_keys = []
+    extended = original.copy()
+
+    for i in range(8):
+        extended.insert(8 * i + 7, extended[8 * i: 8 * i + 7].count() % 2 == 0)
+
+    c = permutate_arr(extended, PC1[:28])
+    d = permutate_arr(extended, PC1[28:])
+
+    for i in range(16):
+        c = cycle_shift_left(c, LEFT_SHIFT[i])
+        d = cycle_shift_left(d, LEFT_SHIFT[i])
+        r_keys.append(permutate_arr(c + d, PC2))
+
+    return r_keys
+
+
+def encrypt(data: bitarray, r_keys: List[bitarray]):
+    permutated = permutate_arr(data, IP)
+    l = permutated[:32]
+    r = permutated[32:]
+
+    for i in r_keys:
+        l, r = r, l ^ f(r, i)
+    return permutate_arr(l + r, FP)
+
+
+def decrypt(data: bitarray, r_keys: List[bitarray]):
+    permutated = permutate_arr(data, IP)
+    l = permutated[:32]
+    r = permutated[32:]
+
+    for i in r_keys:
+        l, r = r ^ f(l, i), l
+    return permutate_arr(l + r, FP)
+
+
+def encrypt_text(text: str, password: str):
+    data = create_bitarray()
+    data.frombytes(text.encode('utf-8'))
+    key = create_bitarray()
+    key.frombytes(password.encode('utf-8'))
+
+    if len(data) % 64 == 0:
+        data.extend((64 - len(data)) * [False])
+
+    if len(key) < 56:
+        key.extend((56 - len(key)) * [False])
+
+    r_keys = generate_keys(key[:56])
+
+    final = create_bitarray()
+
+    for i in range(len(data) // 64):
+        final.extend(encrypt(data[i * 64: (i + 1) * 64], r_keys))
+    return final.tobytes()
+
+
+def decrypt_text(encrypted_bytes: bytes, password: str):
+    data = create_bitarray()
+    data.frombytes(encrypted_bytes)
+    key = create_bitarray()
+    key.frombytes(password.encode('utf-8'))
+
+    if len(key) < 56:
+        key.extend((56 - len(key)) * [False])
+
+    r_keys = list(reversed(generate_keys(key[:56])))
+
+    final = create_bitarray()
+
+    for i in range(len(data) // 64):
+        final.extend(encrypt(data[i * 64: (i + 1) * 64], r_keys))
+    return final.tobytes()
+
+text = 'KEKISTAN WILL WIN THE BATTLE'
+password = 'KEKEKEKEKEKEKEKEKEK'
+print("TEXT ", text.encode('utf-8'), " PASSWORD ", password)
+enc_text = encrypt_text(text, password)
+print("ENCRYPTED ", enc_text)
+print("DECRYPTED ", decrypt_text(enc_text, password))
+
+# data = create_bitarray()
+# data.frombytes(bytes([13, 42, 81, 16, 50, 11, 0, 3]))
+# key = create_bitarray()
+# key.frombytes(bytes([13, 11, 16, 33, 11, 4, 12]))
+# print("DATA ", data, " KEY ", key)
+#
+# encrypted = encrypt(data, key)
+# print("ENCRYPTED ", encrypted)
+#
+# decrypted = decrypt(encrypted, key)
+# print("DECRYPTED ", decrypted)
